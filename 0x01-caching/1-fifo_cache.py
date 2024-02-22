c609948c977ef5966f50a58f23c62d9b6f70b67d
Barniva <barnabasva1@gmail.com>
2024-02-22 22:32:58 +0300

    FIFO caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,45 +1,37 @@
 #!/usr/bin/env python3
-"""Task 1: Simple pagination.
-"""
 
-import csv
-import math
-from typing import List, Tuple
+'''Task 1: FIFO caching
+'''
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Retrieves the index range from a given page and page size.
-    """
-
-    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
+from collections import OrderedDict
+from base_caching import BaseCaching
 
 
-class Server:
-    """Server class to paginate a database of popular baby names.
-    """
-    DATA_FILE = "Popular_Baby_Names.csv"
+class FIFOCache(BaseCaching):
+    '''A class `FIFOCache` that inherits from
+       `BaseCaching` and is a caching system.
+    '''
 
     def __init__(self):
-        self.__dataset = None
+        super().__init__()
+        self.cache_data = OrderedDict()
 
-    def dataset(self) -> List[List]:
-        """Cached dataset
-        """
-        if self.__dataset is None:
-            with open(self.DATA_FILE) as f:
-                reader = csv.reader(f)
-                dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
+    def put(self, key, item):
+        '''assign to the dictionary `self.cache_data` the
+           `item` value for the key `key`
+        '''
 
-        return self.__dataset
+        if key is None or item is None:
+            return
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Retrieves a page of data.
-        """
-        assert type(page) == int and type(page_size) == int
-        assert page > 0 and page_size > 0
-        start, end = index_range(page, page_size)
-        data = self.dataset()
-        if start > len(data):
-            return []
-        return data[start:end]
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {first_key}")
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        '''return the value in `self.cache_data` linked to `key`
+        '''
+        return self.cache_data.get(key, None)
