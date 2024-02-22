a1abf07eb836efeb157f5f5744f2f3ff91c6e286
Barniva <barnabasva1@gmail.com>
2024-02-22 22:33:32 +0300

    FIFO caching

--- 0x01-caching/1-main.py
+++ 0x01-caching/1-main.py
@@ -1,28 +1,16 @@
-#!/usr/bin/env python3
-"""
-Main file
-"""
+#!/usr/bin/python3
+""" 1-main """
+FIFOCache = __import__('1-fifo_cache').FIFOCache
 
-Server = __import__('1-simple_pagination').Server
-
-server = Server()
-
-try:
-    should_err = server.get_page(-10, 2)
-except AssertionError:
-    print("AssertionError raised with negative values")
-
-try:
-    should_err = server.get_page(0, 0)
-except AssertionError:
-    print("AssertionError raised with 0")
-
-try:
-    should_err = server.get_page(2, 'Bob')
-except AssertionError:
-    print("AssertionError raised when page and/or page_size are not ints")
-
-
-print(server.get_page(1, 3))
-print(server.get_page(3, 2))
-print(server.get_page(3000, 100))
+my_cache = FIFOCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.put("D", "School")
+my_cache.print_cache()
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+my_cache.put("C", "Street")
+my_cache.print_cache()
+my_cache.put("F", "Mission")
+my_cache.print_cache()
